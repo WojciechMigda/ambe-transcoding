ea4cd4532d64a6872418195c34f400f47afa3d27
Wojciech Migda <wojtek.golf@interia.pl>
2019-10-09 22:50:11 +0200

    Fork toolchain file into soft- and hard-float

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+extern "C"
+{
+#include "apcop2.h"
+}
+
 int main()
 {
-    std::cout << "Hello ARM!\n";
+    std::cout << "Hello AMBE!\n";
+
+    ENCSTATE_T es;
+
+    ambe_init_enc(&es, APCO_HR_MODE, 1);
 }
